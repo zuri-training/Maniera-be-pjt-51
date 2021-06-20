e34b1b4bff18aab28ee527bc8f197b155e012b2c
Otunba Marcus <otunbamarcusademola@gmail.com>
2021-06-20 21:32:01 +0100

    signin and signup authentication added

---
+++ src/models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const UserSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: "String",
+      enum: ["designer", "customer"],
+      default: "customer",
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("user", UserSchema);
