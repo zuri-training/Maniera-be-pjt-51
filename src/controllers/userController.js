25d9e1ceb9ca335cc75034a8e3097a6d72e3510e
Otunba Marcus <otunbamarcusademola@gmail.com>
2021-06-24 16:19:34 +0100

    added seller's form

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -15,7 +15,7 @@ exports.signupController = async (req, res) => {
   try {
     // Check if user exists
     const user = await User.findOne({ email });
-    if (user) return res.status(400).json({ error: "Email already exists" });
+    if (user) return res.status(400).json({ error: "Email already exists." });
 
     // create an instance of the user
     const newUser = new User({
