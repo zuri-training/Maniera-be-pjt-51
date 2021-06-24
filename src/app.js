7be8709f0d04738d4fad7be20caaecf4274dea82
Samuel Osinloye <psalmueloye@gmail.com>
2021-06-24 10:11:07 +0100

    cors added

--- src/app.js
+++ src/app.js
@@ -11,12 +11,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use((req, res) => {
-  res.status(200).json({
-    message: "Horray, we are live!!!",
-  });
-});
-
 app.use("/api/auth", authRoutes);
 
 module.exports = app;
