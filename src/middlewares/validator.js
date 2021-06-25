20f70705d830eb96bfe76a77e7296ab98e7c56aa
Otunba Marcus <otunbamarcusademola@gmail.com>
2021-06-25 18:33:41 +0100

    validated password input and validated sellers form

--- src/middlewares/validator.js
+++ src/middlewares/validator.js
@@ -5,7 +5,7 @@ exports.signupValidator = [
   check("email", "Invalid email").isEmail().normalizeEmail(),
   check(
     "password",
-    "Password should be combination of one uppercase , one lower case, one special character, one digit and min 8",
+    "Password should have at least one uppercase , one lower case, one special character, one digit and min 8",
   ).matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/, "i"),
 ];
 
