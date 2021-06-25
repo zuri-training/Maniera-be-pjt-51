fe4c5179e8336f59c86b074b11461b2a86f86baa
Otunba Marcus <otunbamarcusademola@gmail.com>
2021-06-25 18:30:05 +0100

    validated password input and validated sellers form

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -11,7 +11,6 @@ const { TOKEN_SECRET, TOKEN_EXPIRY } = process.env;
  */
 exports.signupController = async (req, res) => {
   const { email, password, firstName, lastName } = req.body;
-  console.log(req.body);
   try {
     // Check if user exists
     const user = await User.findOne({ email });
