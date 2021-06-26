25f69552ecea05c8a4cd09dd537dc5bf4fea5a66
Samuel Osinloye <psalmueloye@gmail.com>
2021-06-26 18:10:05 +0100

    Maniera homepage

--- src/middlewares/validator.js
+++ src/middlewares/validator.js
@@ -2,18 +2,10 @@
 const { check, validationResult } = require("express-validator");
 
 // signup validator
-exports.signupValidator = [
-  check("email", "Invalid email").isEmail().normalizeEmail(),
-  check(
-    "password",
-    "Password should have at least one uppercase , one lowercase, one special character, one digit and minimum of 8",
-  ).matches(/^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/),
-];
+exports.signupValidator = [check("email", "Invalid email").isEmail().normalizeEmail()];
 
 // signin validator
 exports.signinValidator = [check("email", "Invalid email").isEmail().normalizeEmail()];
-
-exports.sellerValidator = [check("email", "Invalid email").isEmail().normalizeEmail()];
 
 // seller validator
 exports.sellerValidator = [
