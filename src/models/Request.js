dda2a24925b04ca97a2b9e3f813dc68a2c0b869e
Samuel Osinloye <psalmueloye@gmail.com>
2021-07-03 23:24:01 +0100

    Added mail service

--- src/models/Request.js
+++ src/models/Request.js
@@ -2,70 +2,82 @@ const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
-const RequestSchema = new Schema({
-  fullName: {
-    type: String,
-    required: true,
-    lowercase: true,
-    trim: true,
+const RequestSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+    gender: {
+      type: String,
+      lowercase: true,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    clothingType: {
+      type: String,
+      required: true,
+    },
+    materialType: {
+      type: String,
+      required: true,
+    },
+    colour: {
+      required: true,
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    imageUrl: {
+      type: String,
+    },
+    cloudinaryId: {
+      type: String,
+    },
+    status: {
+      type: Boolean,
+      default: false,
+    },
+    comment: {
+      type: String,
+    },
+    address: {
+      houseNumber: {
+        type: String,
+        required: true,
+      },
+      streetAddress: {
+        type: String,
+        required: true,
+      },
+      city: {
+        type: String,
+        required: true,
+      },
+      state: {
+        type: String,
+        required: true,
+      },
+      country: {
+        type: String,
+        required: true,
+      },
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    lowercase: true,
-    trim: true,
-  },
-  gender: {
-    type: String,
-    lowercase: true,
-    required: true,
-  },
-  phoneNumber: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  clothingType: {
-    type: String,
-    required: true,
-  },
-  materialType: {
-    type: String,
-    required: true,
-  },
-  colour: {
-    required: true,
-    type: String,
-  },
-  description: {
-    type: String,
-  },
-  imageUrl: {
-    type: String,
-  },
-  cloudinaryId:{
-    type: String,
-  },
-  houseNumber: {
-    type: String,
-    required: true,
-  },
-  streetAddress: {
-    type: String,
-    required: true,
-  },
-  city: {
-    type: String,
-    required: true,
-  },
-  state: {
-    type: String,
-    required: true,
-  },
-  country: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true },
+);
 
 module.exports = mongoose.model("request", RequestSchema);
