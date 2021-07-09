ebb25a23529aacadc39ae928ff21ee18de5039d1
Samuel Osinloye <psalmueloye@gmail.com>
2021-07-09 15:48:17 +0100

    deployment

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require("cors");
+//const cors = require("cors");
 const limitter = require("express-rate-limit");
 const compression = require("compression");
 
@@ -16,11 +16,18 @@ require("dotenv").config();
 
 const dbSetup = require("./database/setup");
 
-const { FRONTEND_DEV_URL } = process.env;
+//const { FRONTEND_DEV_URL } = process.env;
 
 dbSetup();
 
 // middlewares
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorizarion');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+  next();
+});
+
 //Limiter setup for DDoS prevention
 app.use(
   limitter({
@@ -40,8 +47,9 @@ app.use(
     threshold: 0,
   }),
 );
-app.use(cors());
-app.use(cors({ origin: FRONTEND_DEV_URL, credentials: true }));
+
+//app.use(cors());
+//app.use(cors({ origin: FRONTEND_DEV_URL, credentials: true }));
 
 app.use(express.json());
 app.use(cookieParser());
